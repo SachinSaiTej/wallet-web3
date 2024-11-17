6a9fb5afa3a218d63becab64e4227bac47585d31
SachinSaiTej <saitejakm2001@gmail.com>
2024-11-17 21:37:15 +0530

    Added Ethereum wallets to the app.

--- src/components/EthereumWallet.tsx
+++ src/components/EthereumWallet.tsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import { mnemonicToSeed } from "bip39";
+import { Wallet } from "ethers";
+import { HDNodeWallet } from "ethers";
+import React from "react";
 
-const EthereumWallet = ({menmonic}:{mnemonic:string}) => {
+const EthereumWallet = ({ mnemonic }: { mnemonic: string }) => {
+  const [addresses, setAddresses] = React.useState<string[]>([]);
+
+  const createWallet = async () => {
+    const seed = await mnemonicToSeed(mnemonic);
+    const derivationPath = `m/44'/60'/${addresses.length}'/0'`;
+    const hdNode = HDNodeWallet.fromSeed(seed);
+    const child = hdNode.derivePath(derivationPath);
+    const privateKey = child.privateKey;
+    const wallet = new Wallet(privateKey);
+    setAddresses([...addresses, wallet.address]);
+  };
+
   return (
     <div style={{ display: "flex", flexDirection: "column" }}>
-        <button>Add Ethereum Wallet</button>
+      <button onClick={createWallet}>Add Ethereum Wallet</button>
+      {addresses.map((address) => (
+        <p>{address}</p>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default EthereumWallet
+export default EthereumWallet;
