6f40216cfefe16451c602333af9bcfcce868fdb9
SachinSaiTej <saitejakm2001@gmail.com>
2024-11-24 21:55:48 +0530

    Added Send Sol section.

--- src/components/Airdrop.tsx
+++ src/components/Airdrop.tsx
@@ -1,5 +1,11 @@
 import { ed25519 } from "@noble/curves/ed25519";
 import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import {
+  LAMPORTS_PER_SOL,
+  PublicKey,
+  SystemProgram,
+  Transaction,
+} from "@solana/web3.js";
 import bs58 from "bs58";
 import React, { useState } from "react";
 
@@ -29,6 +35,23 @@ const Airdrop = () => {
     alert(`success!. Message signature: ${bs58.encode(signature)}`);
   };
 
+  const sendToken = async () => {
+    let to = document.getElementById("to")!.value;
+    let amount = document.getElementById("amount")!.value;
+    const transaction = new Transaction();
+    transaction.add(
+      SystemProgram.transfer({
+        fromPubkey: wallet.publicKey,
+        toPubkey: new PublicKey(to),
+        lamports: amount * LAMPORTS_PER_SOL,
+      })
+    );
+
+    const x = await wallet.sendTransaction(transaction, connection);
+    alert(`Sent ${amount} SOL to ${to}`);
+    console.log("Transaction: ", x);
+  };
+
   const getBalance = async () => {
     if (wallet.publicKey) {
       const balance = await connection.getBalance(wallet.publicKey);
@@ -43,8 +66,15 @@ const Airdrop = () => {
       {wallet.publicKey && <div>Solana Balance : {balance}</div>}
       {/* Sign Transaction */}
       <div>
+        <h1>Sign a Message</h1>
         <input type="text" placeholder="Message" id="message" />
         <button onClick={signMessageClick}>Sign Message</button>
+      </div>
+      <div>
+        <h1>Send Token</h1>
+        <input type="text" placeholder="To" id="to" />
+        <input type="text" placeholder="Amount" id="amount" />
+        <button onClick={sendToken}>Send</button>
       </div>
     </div>
   );
